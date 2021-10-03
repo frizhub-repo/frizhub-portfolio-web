33cb3f2bddb4ce1ed1a1270650e4019ba3b83317
se-Haroon <haroon.rasheed@frizhub.com>
2021-10-03 18:44:09 +0500

    feature: added people carousel

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Layout from "../components/layout";
 import Hero from "../components/hero/hero";
 import CaseStudies from "../components/caseStudies/caseStudies";
+import CustomCarousel from "../components/common/customCarousel/customCarousel";
+import styles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
@@ -8,6 +10,18 @@ export default function Home() {
       <Hero />
       <hr />
       <CaseStudies />
+      <section className={styles.container}>
+        <h1 className={styles.full_width_text}>It's all about the people</h1>
+        <CustomCarousel
+          slides={Array(10).fill({
+            img: "images/members/haroon.jpeg",
+            overlay: {
+              main: "Haroon Rasheed",
+              title: "ReactJS Developer",
+            },
+          })}
+        />
+      </section>
     </Layout>
   );
 }
