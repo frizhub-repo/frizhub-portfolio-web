e0612befda4bc4669d57eaff4e2d6936820beab1
se-Haroon <haroon.rasheed@frizhub.com>
2021-10-14 18:46:57 +0500

    fix: fixed animations + added new animations in case studies

--- components/caseStudies/secondaryCaseStudies.js
+++ components/caseStudies/secondaryCaseStudies.js
@@ -1,5 +1,6 @@
 import ProductCard from "../common/productCard/productCard";
 import styles from "./caseStudies.module.css";
+import Fade from "react-reveal/Fade";
 
 export default function SecondaryCaseStudies() {
   return (
@@ -11,57 +12,65 @@ export default function SecondaryCaseStudies() {
               A Team of a Product Engineers That Help You{" "}
               <span className={styles.shapeText}>Shape Ideas</span>
             </h1>
-            <ProductCard
-              logo="/svgs/contanuity-logo.svg"
-              bgColor="#006ED3"
-              specs={[
-                "Architechture",
-                "Full Stack Development",
-                "Brad Desgin UX Design",
-              ]}
-              overview="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
-              bottomSvg="/svgs/studies/study1.svg"
-              linkTo="case-studies/contanuity"
-            />
-            <ProductCard
-              logo="/svgs/contanuity-logo.svg"
-              bgColor="#F9F9F9"
-              specs={[
-                "Architechture",
-                "Full Stack Development",
-                "Brad Desgin UX Design",
-              ]}
-              overview="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
-              bottomSvg="/svgs/studies/study2.svg"
-              linkTo="case-studies/contanuity"
-              color="black"
-            />
+            <Fade bottom>
+              <ProductCard
+                logo="/svgs/contanuity-logo.svg"
+                bgColor="#006ED3"
+                specs={[
+                  "Architechture",
+                  "Full Stack Development",
+                  "Brad Desgin UX Design",
+                ]}
+                overview="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+                bottomSvg="/svgs/studies/study1.svg"
+                linkTo="case-studies/contanuity"
+              />
+            </Fade>
+            <Fade bottom>
+              <ProductCard
+                logo="/svgs/contanuity-logo.svg"
+                bgColor="#F9F9F9"
+                specs={[
+                  "Architechture",
+                  "Full Stack Development",
+                  "Brad Desgin UX Design",
+                ]}
+                overview="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+                bottomSvg="/svgs/studies/study2.svg"
+                linkTo="case-studies/contanuity"
+                color="black"
+              />
+            </Fade>
           </div>
           <div className={styles.rightStudies}>
-            <ProductCard
-              logo="/svgs/contanuity-logo.svg"
-              bgColor="#279F97"
-              specs={[
-                "Architechture",
-                "Full Stack Development",
-                "Brad Desgin UX Design",
-              ]}
-              overview="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
-              bottomSvg="/svgs/studies/study3.svg"
-              linkTo="case-studies/contanuity"
-            />
-            <ProductCard
-              logo="/svgs/contanuity-logo.svg"
-              bgColor="#FA83A0"
-              specs={[
-                "Architechture",
-                "Full Stack Development",
-                "Brad Desgin UX Design",
-              ]}
-              overview="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
-              bottomSvg="/svgs/studies/study4.svg"
-              linkTo="case-studies/contanuity"
-            />
+            <Fade bottom>
+              <ProductCard
+                logo="/svgs/contanuity-logo.svg"
+                bgColor="#279F97"
+                specs={[
+                  "Architechture",
+                  "Full Stack Development",
+                  "Brad Desgin UX Design",
+                ]}
+                overview="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+                bottomSvg="/svgs/studies/study3.svg"
+                linkTo="case-studies/contanuity"
+              />
+            </Fade>
+            <Fade bottom>
+              <ProductCard
+                logo="/svgs/contanuity-logo.svg"
+                bgColor="#FA83A0"
+                specs={[
+                  "Architechture",
+                  "Full Stack Development",
+                  "Brad Desgin UX Design",
+                ]}
+                overview="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+                bottomSvg="/svgs/studies/study4.svg"
+                linkTo="case-studies/contanuity"
+              />
+            </Fade>
           </div>
         </div>
       </div>
