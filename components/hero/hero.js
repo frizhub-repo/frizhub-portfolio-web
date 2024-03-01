aea0b98132adb5f843f2f3de506605d4d55c7757
azaz <azaz@192.168.1.3>
2024-03-01 06:03:51 +0500

    Animation added

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -3,7 +3,7 @@ export default function Hero() {
   return (
     <div className={styles.container}>
       <div>
-        <h1 className={styles.header}><span className={styles.uniqueText}>Code</span> with Confidence</h1>
+        <h1 className={styles.header}><span className={styles.codeText}>Code</span> with Confidence</h1>
         <h1 className={styles.header}> Soar with Success</h1>
         <ul className={styles.propList}>
           <li className={styles.prop}>
