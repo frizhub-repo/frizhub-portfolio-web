85bb5b9fcf9cfea9301cae0899d95d51e5f0d873
azaz <azaz@MacBook-Pro.local>
2024-05-09 18:02:09 +0500

    Added top developers badge

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -15,13 +15,18 @@ export default function Hero() {
           <li className={styles.prop}>
             <span>&#129351; </span> Beautifully Engineer Everything
           </li>
-        </ul>
+          <div className={styles.motiveTextContainer}>
+          <div><a target="_blank" href="https://www.topdevelopers.co/directory/social-media-management-companies">
+              <img className={styles.badge} src="https://www.topdevelopers.co/assets/badges/app-2023/Software-Development/badge-top-software-developers-2023.png" alt="Social Media Management Companies" title="Social Media Management Companies"/>
+          </a></div>
+                
+          <div><a target="_blank" href="https://www.topdevelopers.co/directory/ecommerce-developers">
+          <img className={styles.badge} src="https://www.topdevelopers.co/assets/badges/app-2023/Ecommerce/rectangle-top-ecommerce-development-companies-2023.png" alt="eCommerce Development Companies" title="eCommerce Development Companies"/>
+          </a></div>
       </div>
-      {/* <div className={styles.motiveTextContainer}>
-        <p className={styles.motiveText}>
-        Ability proceeds from a fashion of skills, knowledge, understanding, and imagination experience.
-        </p>
-      </div> */}
+        </ul>
+        
+      </div>
     </div>
   );
 }
