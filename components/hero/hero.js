416ee13c45a226ee7463401994b3fd20c7cee9ce
Atif <atif.sik252@gmail.com>
2024-12-11 23:15:55 +0500

    Image: Use rel=noreferrer to mitigate security risk in older browser

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -3,7 +3,9 @@ export default function Hero() {
   return (
     <div className={styles.container}>
       <div>
-        <h1 className={styles.header}><span className={styles.codeText}>Code</span> with Confidence</h1>
+        <h1 className={styles.header}>
+          <span className={styles.codeText}>Code</span> with Confidence
+        </h1>
         <h1 className={styles.header}> Soar with Success</h1>
         <ul className={styles.propList}>
           <li className={styles.prop}>
@@ -16,15 +18,36 @@ export default function Hero() {
             <span>&#129351; </span> Beautifully Engineer Everything
           </li>
           <div className={styles.motiveTextContainer}>
-          <div><a target="_blank" href="https://www.topdevelopers.co/directory/social-media-management-companies">
-            <img className={styles.badge} src="https://www.topdevelopers.co/assets/badges/app-2023/Software-Development/badge-top-software-developers-2023.png" alt="Social Media Management Companies" title="Social Media Management Companies"/>
-          </a></div>                
-          <div><a target="_blank" href="https://www.topdevelopers.co/directory/ecommerce-developers">
-            <img className={styles.badge} src="https://www.topdevelopers.co/assets/badges/app-2023/Ecommerce/rectangle-top-ecommerce-development-companies-2023.png" alt="eCommerce Development Companies" title="eCommerce Development Companies"/>
-          </a></div>
-      </div>
+            <div>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.topdevelopers.co/directory/social-media-management-companies"
+              >
+                <img
+                  className={styles.badge}
+                  src="https://www.topdevelopers.co/assets/badges/app-2023/Software-Development/badge-top-software-developers-2023.png"
+                  alt="Social Media Management Companies"
+                  title="Social Media Management Companies"
+                />
+              </a>
+            </div>
+            <div>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.topdevelopers.co/directory/ecommerce-developers"
+              >
+                <img
+                  className={styles.badge}
+                  src="https://www.topdevelopers.co/assets/badges/app-2023/Ecommerce/rectangle-top-ecommerce-development-companies-2023.png"
+                  alt="eCommerce Development Companies"
+                  title="eCommerce Development Companies"
+                />
+              </a>
+            </div>
+          </div>
         </ul>
-        
       </div>
     </div>
   );
